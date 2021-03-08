cabfedc01c58a34b9535d71fc09e1d5f08dd1cf3
callkend <77285430+callkend@users.noreply.github.com>
2021-03-08 09:03:45 -0700

    Fixed item box fill, and read

--- StansGroceryForm.vb
+++ StansGroceryForm.vb
@@ -4,15 +4,17 @@
     End Sub
 
     Sub ReadEntireFile(grabItem As String)
-        Dim scan As String = ""
+        Dim scan As String = "nothing"
         Dim itemInfo As String
         If grabItem = "" Then
             Try
                 FileOpen(1, "Grocery.txt", OpenMode.Input)
                 Do Until EOF(1)
                     Input(1, scan)
-                    If (InStrRev(scan, "ITM") = 5 Or InStrRev(scan, "ITM") = 3) Then
+                    If InStrRev(scan, "ITM") <= 5 And InStrRev(scan, "ITM") <> 0 Then
                         scan = Replace(scan, "$$ITM", "")
+                        scan = Replace(scan, vbLf, "")
+                        scan = Replace(scan, """", "")
                         DisplayListBox.Items.Add(scan)
                         ComboBox1.Items.Add(scan)
                     End If
@@ -28,7 +30,9 @@
                     Input(1, scan)
                     If (InStrRev(scan, grabItem) >= 1) Then
                         scan = Replace(scan, "$$ITM", "")
-                        itemInfo = $"Item:{Mid(scan, 2)}" + vbCrLf
+                        scan = Replace(scan, vbLf, "")
+                        scan = Replace(scan, """", "")
+                        itemInfo = $"Item:{Mid(scan, 1)}" + vbCrLf
                         Input(1, scan)
                         scan = Replace(scan, "##LOC", "")
                         itemInfo += $"Location:{scan}" + vbCrLf
@@ -40,6 +44,7 @@
                         Return
                     End If
                 Loop
+                FileClose()
             Catch ex As System.IO.FileNotFoundException
 
             End Try
@@ -49,21 +54,21 @@
 
     Private Sub DisplayListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DisplayListBox.SelectedIndexChanged
         Dim itemSelect As String = DisplayListBox.GetItemText(DisplayListBox.SelectedItem)
-        itemSelect = Mid(itemSelect, 5, Len(itemSelect))
+        itemSelect = Mid(itemSelect, 1, Len(itemSelect))
         ReadEntireFile(itemSelect)
 
     End Sub
 
     Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
         Dim itemSelect As String = ComboBox1.GetItemText(ComboBox1.SelectedItem)
-        itemSelect = Mid(itemSelect, 5, Len(itemSelect))
+        itemSelect = Mid(itemSelect, 1, Len(itemSelect))
         ReadEntireFile(itemSelect)
 
     End Sub
 
     Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
         Dim itemSelect As String = TextBox1.Text
-        itemSelect = Mid(itemSelect, 5, Len(itemSelect))
+        itemSelect = Mid(itemSelect, 1, Len(itemSelect))
         ReadEntireFile(itemSelect)
     End Sub
 End Class
